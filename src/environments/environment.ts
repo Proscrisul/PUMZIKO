98fe32be598864a8b116e68aed70ce4d678a5f27
sacalivin <obirisacalivin123@gmail.com>
2026-09-03 20:09:47 +0300

    updated url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:8000/api',
+  apiUrl: 'https://api.pumziko.org/api',
   appName: 'Pumziko',
   siteUrl: 'https://pumziko.org',
   version: '1.0.0',
